79e19e2698ee1c5e3d53f44ac1d68db277cb6ad2
XSamuel <131273393+siming323@users.noreply.github.com>
2025-02-10 05:22:57 -0500

    verify

--- src/main/java/com/example/backend/config/WebConfig.java
+++ src/main/java/com/example/backend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://18.227.46.51")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
